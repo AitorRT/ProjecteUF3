e395b05d20eb48a4f32d8041c429948126719e51
linux <linux@debian10.4>
2021-03-09 18:35:12 +0100

    second commit

--- resources/views/videosview.blade.php
+++ resources/views/videosview.blade.php
@@ -12,7 +12,63 @@
             </div>
         </div>
     </section>
+    <style>
+        * {
+            box-sizing: border-box;
+        }
 
+        body {
+            margin: 0;
+            font-size: 17px;
+        }
+
+        #myVideo {
+            position: fixed;
+            right: 0;
+            bottom: 0;
+            min-width: 100%;
+            min-height: 100%;
+        }
+
+        .content {
+            position: fixed;
+            bottom: 0;
+            background: rgba(0, 0, 0, 0.5);
+            color: #f1f1f1;
+            width: 100%;
+            padding: 20px;
+        }
+
+        #myBtn {
+            width: 200px;
+            font-size: 18px;
+            padding: 10px;
+            border: none;
+            background: #000;
+            color: #fff;
+            cursor: pointer;
+        }
+
+        #myBtn:hover {
+            background: #ddd;
+            color: black;
+        }
+        a:link, a:visited {
+            width: 200px;
+            font-size: 18px;
+            padding: 10px;
+            background-color: #f44336;
+            color: white;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            float: right;
+        }
+
+        a:hover, a:active {
+            background-color: red;
+        }
+    </style>
 
     <video autoplay muted loop id="myVideo">
         @if($video->route!=null)<source src="{{asset('storage/'.$video->route)}}" type="video/mp4">@endif
